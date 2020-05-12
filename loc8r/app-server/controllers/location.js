4b809c9bf75418a5ed9511e4c93d29c4d0d7ef27
lenovo <sharmatushar759@gmail.com>
2020-05-12 13:04:21 +0530

    Adding jade view

---
+++ loc8r/app-server/controllers/location.js
@@ -0,0 +1,42 @@
+
+module.exports.locationInfo = (req,res) => {
+
+    res.render('location-info',{title:'Location Info'});
+
+};
+
+module.exports.homeList = (req,res) => {
+
+    res.render('location-list', { title : 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+        title: 'Loc8r',
+        strapline: 'Find places to work with wifi near you!'
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+            },{
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+            },{
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+            }]
+        
+        });
+    
+};
+
+module.exports.addReview = (req,res)=>{
+
+    res.render('add-review',{title:'Add Review'});
+};
---
+++ loc8r/app-server/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const ctrlOthers = require('../controllers/others');
+const ctrlLocation =  require('../controllers/location');
+//console.log(express);
+/* GET home page. */
+
+
+router.get('/about', ctrlOthers.about);
+router.get('/location',ctrlLocation.locationInfo);
+router.get('/location/review/new',ctrlLocation.addReview);
+router.get('',ctrlLocation.homeList);
+
+
+module.exports = router;
